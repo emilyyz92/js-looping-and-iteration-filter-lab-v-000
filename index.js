d1a66d35a75e17c06d327ffe0416b3add6ce890e
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 18:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ function findMatching(array, name) {
 }
 
 function fuzzyMatch(array, string) {
-  function matchFuzzy(string) {
-    for (let n = 0; n <= string.length; n++) {
-      array[n] === string[n]
-    }
-  }
   const newArray = array.filter(word => word.slice(0, string.length).toLowerCase === string.toLowerCase);
   return newArray;
 }
