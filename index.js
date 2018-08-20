852335c0f24e5f7a3a01a334c32a08ddce3a5f05
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 17:39:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  const newarray = array.filter(word => word === name); 
+  const newarray = array.filter(word => word === name);
+  return newarray;
 }
