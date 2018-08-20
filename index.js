845c39da0507738779998522e18f2d645800e7ee
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 18:54:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function fuzzyMatch(array, string) {
   function match(word) {
     return word.slice(0, string.length).toLowerCase === string.toLowerCase;
   }
-  const newArray = array.filter(word => match(word));
+  const newArray = array.filter(match);
   return newArray;
 }
