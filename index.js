2023b00133425c8ef5fc6f3bbe9255b25756556e
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 17:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  const newarray = array.filter(word => word.toLowerCase() === name.tolowercase());
+  const newarray = array.filter(word => word.toLowerCase() === name.toLowerCase());
   return newarray;
 }
